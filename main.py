c81e471fd609ca61cd8492cd2ca9151cc7ede197
karlis <karlis@karlis-desktop.localdomain>
2010-10-22 11:12:01 +0000

    added event splitting into pages

--- main.py
+++ main.py
@@ -6,20 +6,27 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 
 class MainPage(webapp.RequestHandler):
-    def get(self):
-		events_query = Event.all().order('-date')
-		events = events_query.fetch(10)
-
-		for event in events :
+    def get(self,page=1):
+        epp = 10 # events per page
+        page = int(page)
+        older = newer = False
+        offset = page*epp-epp
+        
+        events_query = Event.all().order('-date')
+        events = events_query.fetch(limit = epp+1, offset = offset)
+        if len(events) == epp+1:
+            older = page+1
+            events.pop() 
+        if offset > 0:
+            newer = page-1
+        for event in events :
 			event.date_display = event.date.strftime('%Y/%m/%d')
+        template_values = {'events':events, 'newer':newer, 'older':older}
 
-		template_values = {'events':events}	
-	
-		path = os.path.join(os.path.dirname(__file__),'Templates/base_pub.html')
-		self.response.out.write(template.render(path,template_values))
+        path = os.path.join(os.path.dirname(__file__),'Templates/base_pub.html')
+        self.response.out.write(template.render(path,template_values))
 
-application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+application = webapp.WSGIApplication([('/', MainPage),('/page/(.*)',MainPage)],
                                      debug=True)
 
 def main():
