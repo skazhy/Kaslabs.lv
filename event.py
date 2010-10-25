cf0b50406dbcab18c3d91e07acc76bdfefbad6d8
Karlis <karlis@kk-desktop.localdomain>
2010-10-25 19:56:25 +0300

    Eror handling on incorrect event ids

--- event.py
+++ event.py
@@ -8,11 +8,16 @@ from google.appengine.ext import webapp
 class EventPage(webapp.RequestHandler):
     def get(self,ev_id):
         ev_id = int(ev_id)
-	event = Event.get_by_id(ev_id)
-	event.date_display = event.date.strftime('%-d. %B, %Y')
-	event.time_display = event.date.strftime('%R')
-        template_values = {'event':event}
-	path = os.path.join(os.path.dirname(__file__),'Templates/public-event.html')
+        event = Event.get_by_id(ev_id)
+        try: 
+			event.date_display = event.date.strftime('%-d. %B, %Y')
+        except AttributeError:
+			id = False
+        else:
+			id = True
+			event.time_display = event.date.strftime('%R')
+        template_values = {'event':event, 'id':id}
+        path = os.path.join(os.path.dirname(__file__),'Templates/public-event.html')
         self.response.out.write(template.render(path,template_values))
 
 application = webapp.WSGIApplication([('/p/(.*)',EventPage)],
