d5988f8c711b55bf5ac2df26a554eb6dadf6833e
Karlis Lauva <skazhy@gmail.com>
2010-11-02 14:10:24 +0000

    Optimized Lightbox for map viewing

--- JS/lightbox.js
+++ JS/lightbox.js
@@ -46,8 +46,8 @@
 //  Configurationl
 //
 LightboxOptions = Object.extend({
-    fileLoadingImage:        'images/loading.gif',     
-    fileBottomNavCloseImage: 'images/closelabel.gif',
+    fileLoadingImage:        '/Img/lb-loading.gif',     
+    fileBottomNavCloseImage: '/Img/lb-close.gif',
 
     overlayOpacity: 0.8,   // controls transparency of shadow overlay
 
