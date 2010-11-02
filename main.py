c156a7b72b9acf9532b71f72b6fc69b2a05bb5b2
Karlis <karlis@kk-desktop.localdomain>
2010-11-02 23:53:47 +0200

    Added memcaching for main page

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
+from google.appengine.api import memcache
 
 class MainPage(webapp.RequestHandler):
-    def get(self,page=1):
+    def get(self):
+        main_page = self.getMain()
+        self.response.out.write(main_page)
+	
+    def getMain(self):
+        main_page = memcache.get("main_page")
+        if main_page is not None:
+            return main_page
+        else:
+            main_page = self.renderMain()
+            memcache.add("main_page",main_page,300)
+            return main_page
+    
+    def renderMain(self):
         today = datetime.date.today()
         events = []
-        m_ev = db.GqlQuery("SELECT * FROM Event WHERE end_date >= :1", today)
-        for event in m_ev:
+        evs = db.GqlQuery("SELECT * FROM Event WHERE end_date >= :1", today)
+        for event in evs:
             events.append(event)
-        s_ev = db.GqlQuery("SELECT * FROM Event WHERE end_date = NULL AND date >= :1", today)
-        for event in s_ev:
+        evs = db.GqlQuery("SELECT * FROM Event WHERE end_date = NULL AND date >= :1", today)
+        for event in evs:
             events.append(event)
         events = sorted(events, key=lambda Event: Event.date) 
         for event in events:
@@ -23,11 +37,11 @@ class MainPage(webapp.RequestHandler):
                 event.divLen = 16*len(event.title)
             event.date_display = event.date.strftime('%R')
         template_values = {'events':events}
-
         path = os.path.join(os.path.dirname(__file__),'Templates/base-pub.html')
-        self.response.out.write(template.render(path,template_values))
+        main_page = template.render(path,template_values)
+        return main_page
 
-application = webapp.WSGIApplication([('/', MainPage),('/page/(.*)',MainPage)],debug=True)
+application = webapp.WSGIApplication([('/', MainPage)],debug=True)
 
 def main():
     run_wsgi_app(application)
