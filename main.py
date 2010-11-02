73caeda5d5d9bc04e297075374cc0f10e9b20310
Karlis Lauva <skazhy@gmail.com>
2010-11-02 12:01:44 +0000

    Displaying started & upcoming events in main page

--- main.py
+++ main.py
@@ -7,30 +7,22 @@ from google.appengine.ext import webapp
 
 class MainPage(webapp.RequestHandler):
     def get(self,page=1):
-        # epp = 10 # pasaakumu skaits lapaa 
-        # page = int(page)
-        # older = newer = False
-        # offset = page*epp-epp
-        events_query = Event.all().filter('date >= ', datetime.date.today()).order('date')
-        #events = events_query.fetch(limit = epp+1, offset = offset)
-        events = events_query.fetch(limit = 100) # shitais jaapaartaisa
-        """
-        if len(events) == epp+1:
-            older = page+1
-            events.pop() 
-        if offset > 0:
-            newer = page-1
-        if len(events) == 0:
-            older=newer=False
-        """
+        today = datetime.date.today()
+        events = []
+        m_ev = db.GqlQuery("SELECT * FROM Event WHERE end_date >= :1", today)
+        for event in m_ev:
+            events.append(event)
+        s_ev = db.GqlQuery("SELECT * FROM Event WHERE end_date = NULL AND date >= :1", today)
+        for event in s_ev:
+            events.append(event)
+        events = sorted(events, key=lambda Event: Event.date) 
         for event in events:
-		if len(event.title) < 15:
-			event.divLen = 240
-		else:
-			event.divLen = 16*len(event.title)
-		event.date_display = event.date.strftime('%R')
+            if len(event.title) < 15:
+                event.divLen = 240
+            else:
+                event.divLen = 16*len(event.title)
+            event.date_display = event.date.strftime('%R')
         template_values = {'events':events}
-	    #template_values = {'events':events, 'newer':newer, 'older':older}
 
         path = os.path.join(os.path.dirname(__file__),'Templates/base-pub.html')
         self.response.out.write(template.render(path,template_values))
